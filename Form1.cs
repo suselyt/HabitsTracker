692b572f3ea72cb029697beb9b893d921261d608
Susely <susely.dev@gmail.com>
2025-08-14 20:28:14 -0500

    Data Source a Checked List

--- Form1.cs
+++ Form1.cs
@@ -1,17 +1,22 @@
-﻿using System;
+﻿using Habits;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace Habits
 {
     public partial class Form1 : Form
     {
+        private readonly HabitManager _habitManager = new HabitManager();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +24,14 @@ namespace Habits
 
         private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkedListBox1.DataSource = _habitManager.Habits;
+            checkedListBox1.DisplayMember = "Name";
+            checkedListBox1.ValueMember = "Id";
+        }
+
     }
 }
